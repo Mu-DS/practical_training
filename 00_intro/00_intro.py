c6c9ce4780fd4ff0ff17a97ba78624364ab3c601
RCCG <r.greif@outlook.com>
2020-08-20 19:47:57 +0200

    Add files via upload

--- 00_intro/00_intro.py
+++ 00_intro/00_intro.py
@@ -6,56 +6,106 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.4.2
+#       jupytext_version: 1.5.2
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
 
-# + [markdown] Collapsed="false"
 # # Why Practical Training is Crucial
 
-# + [markdown] Collapsed="false"
 # ## Why bridging the gap is important
 #
 # <img src="https://preview.redd.it/zj4noiwbf7g31.jpg?width=640&crop=smart&auto=webp&s=75069427a53b85daa79284f096329d5a70c34719" width=300>
 #
 # <img src="https://external-preview.redd.it/2htFTGm3vrPB41z1b_jHeZ122-lGhMK6aKphMGwShXA.jpg?width=640&crop=smart&auto=webp&s=1b05de40142c59d22e42463b1804e187e550f46a" width=300>
 
-# + [markdown] Collapsed="false"
-# ## Gap between theoretical knowledge and being able to do it
+# There are plenty of expensive courses, thick books, and jaded postdocs telling you how to do things in theory - and that's great! 
+
+# But... how do you get there?
+
+# Let's play a little game, can you tell me how to do each of these?
+
+# | Problem                                                | Implementation |
+# |--------------------------------------------------------|----------------|
+# | Develop this new framework over the next 6 months      | ? |
+# | Adding this feature will take a while                  | ? |
+# | The code you're writing is turnign into a monster      | ? |
+# | Hmmm this Jupyter notebook is gettign too long         | ? |
+# | Writing documentation is too troublesome               | ? |
+# | The code of the other developer looks terrible         | ? |
+# | "Why is there a super() in here???"                    | ? |
+# | "You know, you should make this script run with a CLI" | ? |
+# | "How are these objects related?"                       | ? |
+# | Chart the structure of your project                    | ? |
+# | Figure out which part is slowing down the code         | ? |
+# | Speed up this NumPy code                               | ? |
+# | This loop is really slow... |  |
+
+# Solution:
+
+# | Problem                                                | Implementation |
+# |--------------------------------------------------------|----------------|
+# | Develop this new framework over the next 6 months      | Agile, Sprint planning, etc. |
+# | Adding this feature will take a while                  | Sprint planning, review process |
+# | The code you're writing is turnign into a monster      | Code architecture, refactoring |
+# | Hmmm this Jupyter notebook is gettign too long         | module architecture |
+# | Writing documentation is too troublesome               | AutoDoc, Docstring creator, etc |
+# | The code of the other developer looks terrible         | code formatter, linting |
+# | "Why is there a super() in here???"                    | Java Developers |
+# | "What are these properties?"                           | Setters and Getters |
+# | "You know, you should make this script run with a CLI" | click |
+# | "How are these objects related?"                       | Coda Analytzr |
+# | "Can you show me how this project is structured?"      | UML, Code Analyzer |
+# | Figure out which part is slowing down the code         | Dynamic Code Analyzer / Profiler |
+# | Speed up this NumPy code                               | Numba |
+# | This loop is really slow....                           | Map(), Numba, Dask |
+# | I should run this in parallel...                       | Multiprocessing, Dask |
+
+# # Is this Course for you?
+
+# <img src="https://blog.codinghorror.com/content/images/uploads/2009/02/6a0120a85dcdae970b012877707a45970c-pi.png" width=320>
+
+# Have you ever gotten...
+
+# + [markdown] jupyter={"outputs_hidden": true}
+# - a shared project in your group, but couldn't figure out what 80\% of the functions or objects did?
+# -
+
+
+# - a code from a previous student/phd/postdoc and though -- WHAT THE F- is this?!
+
+# - a bachelor student to use your code and only gotten stupid questions from them?
+
+# I hate to break it to you, but you also write bad code
+
+# We all write bad code, and the point is not to write perfect code, but to write less bad code.
 #
-# It all sounds easy enough, but how do I actually do this?
-#
-# How do you build a piece of code that should be used by other people?
+# Just a world with less bad code. That's the dream.
 
-# + Collapsed="false"
+# # Exercise
 
+# - Pair up in groups of 2 or 3
+# - Show the other person your last opened python code you wrote
+# - Spend 5 minutes trying to unsderstand it
+# - Discuss the code
 
-# + [markdown] Collapsed="false"
-# # Research Software Projects
-# https://swcarpentry.github.io/managing-research-software-projects/
+# # Overview of the Course
 
-# + Collapsed="false"
-
-
-# + Collapsed="false"
-
-
-# + [markdown] Collapsed="false"
-# # Overview
-
-# + [markdown] Collapsed="false"
 # 1. Fundamentals of Production Code
+#    - Workflow Organization
 #    - Environments
-#    - Code Style
+#    - Code Style and Formatters
 #    - Design Patterns
 #    - Thinking Functionally
+#    - Module Architecture
+#    - CLI Interfaces
 # 2. Data Management Fundamentals
 #     - Pre-SQL
 #     - SQL
 #     - NoSQL
+#     - Graph Databases
 # 3. Continuous Integration Pipeline
 #     - Git
 #     - Unit Tests
@@ -66,8 +116,7 @@
 #     - Coding
 # 5. Processing Data Efficiently
 #     - Tensorflow
+#     - Network Architeictures & Applications
+#     - Slurm
 #     - Numba
 #     - Dask
-
-# + Collapsed="false"
-
